9bfd0de6e95a9207c9927e0f61d0b21703485eb1
masahiro-fujita <sketchbook.fm@gmail.com>
2020-12-13 22:54:18 +0900

    first commit

---
+++ components/card.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card class="card mb-5">
+    <nuxt-link
+      :to="{ name: 'blog-slug'}"
+      class="wrapper"
+    >
+      <v-card-title>記事タイトル</v-card-title>
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+      </v-row>
+      <div>記事の概要</div>
+    </v-card-text>
+    </nuxt-link>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  text-decoration: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="index">
+    <card
+      v-for="i in 5"
+      :key="i"
+    />
+  </v-container>
+</template>
+
+<script>
+import Card from '~/components/card.vue'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
